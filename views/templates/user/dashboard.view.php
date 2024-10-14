07eca85594de374ca3c26b4ec1184cd6bd31bb27
ChrisGreta <c.leydier@velay.greta.fr>
2024-10-14 16:45:32 +0200

    Dashboard mini modif

--- views/templates/user/dashboard.view.php
+++ views/templates/user/dashboard.view.php
@@ -39,19 +39,9 @@
                             </div>
                     
                         </h5>
-                        <h5 class="mb-2 font-xl font-bold tracking-tight text-gray-900 dark:text-white">Débuté le: </h5>
+                        <h5 class="mb-2 font-xl font-bold tracking-tight text-gray-900 dark:text-white">Débuté le: <?= $do['session_debut'] ?></h5>
                         
-                        <?php if (1 == 1){
-                           
-                        ?>
-                            <h5 class="mb-2 font-xl font-bold tracking-tight text-gray-900 dark:text-white">Terminé le: </h5>
-                        <?php
-                        }else{
-                        ?>
-                            <h5 class="mb-2 font-xl font-bold tracking-tight text-gray-900 dark:text-white">Dernière MAJ: </h5>
-                        <?php
-                        }
-                        ?>
+
                         
                         <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
                             <div class="bg-<?= ( $doprogress == 1 ? "green" : "blue");?>-600 h-6 rounded-full" style="width: <?= $doprogress*100;?>%">
@@ -126,8 +116,8 @@
                 ?>             
                 <div class="max-w-sm p-6 w-72 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <br/>
-                    <a href="index.php?page=dc&session_id=new" class="inline-flex w-full items-center px-1 py-2 text-sm text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Débuter un nouveau test
+                    <a href="index.php?page=step0" class="inline-flex w-full items-center px-1 py-2 text-sm text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Demander un devis assurance dommage ouvrage
                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                         </svg>
